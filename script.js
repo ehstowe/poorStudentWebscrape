92e4e60269920f96b6b14900652315175b8e5197
ehstowe <47224648+ehstowe@users.noreply.github.com>
2019-02-21 15:18:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.innerHTML("var majors = d3.selectAll("[class='fusion-builder-row fusion-row '] a, .title-heading-center").nodes().forEach(function(e){console.log(e.innerText);});")
-document.innerHTML("var csc = d3.selectAll("#main li").nodes().forEach(function(e){console.log(e.innerText);})")
+document.getElementById("here").innerHTML("var majors = d3.selectAll("[class='fusion-builder-row fusion-row '] a, .title-heading-center").nodes().forEach(function(e){console.log(e.innerText);});")
+document.getElementById("here").innerHTML("var csc = d3.selectAll("#main li").nodes().forEach(function(e){console.log(e.innerText);})")
