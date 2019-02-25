62a1bb3790c4eecbcdd4f6fa7ecf4cfc22bc6b21
ehstowe <47224648+ehstowe@users.noreply.github.com>
2019-02-25 18:51:08 -0500

    Update cscClasses.js

--- cscClasses.js
+++ cscClasses.js
@@ -1,1 +1,1 @@
-document.getElementById("here2").innerHTML("var csc = d3.selectAll('#main li').nodes().forEach(function(e){console.log(e.innerText);})")
+"var csc = d3.selectAll('#main li').nodes().forEach(function(e){console.log(e.innerText);})"
