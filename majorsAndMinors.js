9631463d332ca735803a413ca9b0a3c9364132f1
ehstowe <47224648+ehstowe@users.noreply.github.com>
2019-02-25 19:01:54 -0500

    Update majorsAndMinors.js

--- majorsAndMinors.js
+++ majorsAndMinors.js
@@ -1,2 +1,6 @@
+var s = document.createElement("script")
+s.src="https://d3js.org/d3.v5.js"
+var body=document.getElementsByTagName("body")[0]
+body.after(s);
 var majors = d3.selectAll('.fusion-builder-row a, .title-heading-center').nodes().forEach(function(e){console.log(e.innerText);});
 
